dc4da6deab346acc5fde1de7503a27a9e6cd7097
Ham woojo <woojo3398@naver.com>
2023-07-13 00:41:31 +0900

    [feat] 딜러의 역할 구현 - 단 한줄의 코드 한줄의 리턴이라도 명확한 일을 표현하기 위해 메소드 구현 - 핵심은 누가 봐도 코드가 무엇을 하는지 명확히 표현할 것 (그 역할은 주석이 아니다) - 의도가 명확한 코드와 변수명, 메소드명이 해야한다.

--- src/main/java/com/blackjack/Dealer.java
+++ src/main/java/com/blackjack/Dealer.java
@@ -1,5 +1,6 @@
 package com.blackjack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -7,13 +8,46 @@ import java.util.List;
 * 카드를 오픈한다. 뽑은 카드를 소유한다.*/
 public class Dealer {
     private List<Card> cards;
-    private static int MaxCount = 16;
+    private static int CAN_RECEIVE_POINT = 16;
+
+    public Dealer(){
+        this.cards = new ArrayList<>();
+    }
 
     public void receiveCard(Card card){
-
+        /* */
+        if(this.isReceiveCard()){
+            this.cards.add(card);
+            this.showCards();
+        }else{
+            System.out.println("카드의 합이 16이상입니다. 더이상 카드를 받을 수 없습니다.");
+        }
     }
+    private boolean isReceiveCard() {
+        return getPointSum() <= CAN_RECEIVE_POINT;
+    }
+
+    private int getPointSum() {
+        int sum = 0;
+        for (Card card : cards) {
+            sum += card.getPoint();
+        }
+        return sum;
+    }
+
     public List<Card> openCards(){
-        return null;
+        return this.cards;
+    }
+    public void showCards(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("현재 보유 카드 목록 \n");
+
+        for(Card card : cards){
+            sb.append(card.toString());
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
     }
 
 }
