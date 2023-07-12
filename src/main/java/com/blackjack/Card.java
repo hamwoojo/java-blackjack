9aa2d6951467dae0bf67d854196b31f2f8e2aec6
Ham woojo <woojo3398@naver.com>
2023-07-13 00:31:48 +0900

    [refactor] CardDeck에 너무 많은 private 메소드로 분리 작업  - Card가 어떻게 변하면서 생성되는 지는 카드덱에게는 중요하지 않다
    하나의 클래스에 private 메소드가 너무 많다면 객체 설계를 다시 고민해볼 것!
    CardDeck 객체의 역할과 책임은 카드 덱을 생성하는 것

--- src/main/java/com/blackjack/Card.java
+++ src/main/java/com/blackjack/Card.java
@@ -12,13 +12,35 @@ public class Card {
     /* A =1,   */
     private String denomination;
 
-    private int Point;
+    private int point;
 
-    /* 인자를 추가한 생성자를 사용한 이유는 pattern과 denomination을 필수임을 강제하기 위해서이다. */
-    public Card(String pattern, String denomination,int point){
+    /* 반복문의 인덱스 */
+    public Card(String pattern,int index){
         this.pattern = pattern;
-        this.denomination = denomination;
+        this.denomination = numberToDenomination(index);
+        this.point = numberToPoint(index);
     }
+
+    private int numberToPoint(int number) {
+        if(number >= 11){
+            return 10;
+        }
+        return number;
+    }
+
+    private String numberToDenomination(int number) {
+        if(number == 1){
+            return "A";
+        } else if (number == 11) {
+            return "J";
+        } else if (number == 12) {
+            return "Q";
+        } else if (number == 13) {
+            return "K";
+        }
+        return String.valueOf(number);
+    }
+
     @Override
     public String toString(){
         return "Card{" + "pattern='" + pattern + ", denomination='" + denomination + '}';
