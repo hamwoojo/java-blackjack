03ed3238ec41a2bbfa2842defe841ad1f552c43e
Ham woojo <woojo3398@naver.com>
2023-07-15 00:56:59 +0900

    [refactor] 쓸모없는 주석 삭제

--- src/main/java/com/blackjack/CardDeck.java
+++ src/main/java/com/blackjack/CardDeck.java
@@ -9,8 +9,6 @@ import java.util.Stack;
 public class CardDeck {
 
     private static final int CARD_COUNT = 13;
-
-    /* List 인터페이스를 활용하면 ArrayList LinkedList 둘다 허용 가능 */
     private Stack<Card> cards;
 
     /* 생성자는 실행 시키는 역할이지 비즈니스 로직을 알 필요는 없다. */
@@ -19,7 +17,6 @@ public class CardDeck {
         Collections.shuffle(cards);
     }
 
-    /* ArrayList는 카드를 draw 했을 떄 해당 인덱스의 언스턴스를 제거하고 다시 카피를 하기 때문에 앞 뒤 노드와 연결되어있는 링크드리스트에 비해 제거의 성능이 좋지 못하다. */
     private Stack<Card> generateCards(){
         Stack<Card> cards = new Stack<>();
         for (Card.Pattern pattern : Card.Pattern.values()) {
